397846688a8e0db09e02c352219b9022eaff5c2c
K-stiles <michealessuman08@gmail.com>
2025-11-11 16:46:53 +0000

    added webpack.development.config.js for for dev environment

---
+++ webpack-bundler/webpack.development.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+  },
+  // module: {
+  //   rules: [{ test: /\.txt$/, use: 'raw-loader' }],
+  // },
+};
