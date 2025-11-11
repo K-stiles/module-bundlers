f97ef5a1ac7bbe0f2607ca85578a84e4a7db639a
K-stiles <michealessuman08@gmail.com>
2025-11-11 17:52:53 +0000

    resolves es6+/jsx with babel-loader

--- webpack-bundler/webpack.development.config.js
+++ webpack-bundler/webpack.development.config.js
@@ -1,13 +1,28 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
+  mode: "development",
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
   },
-  // module: {
-  //   rules: [{ test: /\.txt$/, use: 'raw-loader' }],
-  // },
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env", // modern JS → older JS
+              "@babel/preset-react", // JSX → JS
+              // "@babel/preset-typescript", // remove TS types
+            ],
+          },
+        },
+      },
+    ],
+  },
 };
