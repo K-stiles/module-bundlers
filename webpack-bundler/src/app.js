229530c668efc0e5b44d19294a963129fbfdee01
K-stiles <michealessuman08@gmail.com>
2025-11-12 00:32:51 +0000

    added asset resource loading

--- webpack-bundler/src/app.js
+++ webpack-bundler/src/app.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import "./app.css"
+import testImage from '../public/test-image.jpeg';
 
 export default function App() {
   return (
-    <div className='container'>App Component</div>
+    <div className='container'>
+      App Component
+      <img src={testImage} alt="Test" />
+    </div>
   )
 }
